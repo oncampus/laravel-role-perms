b65af725ceccacec85f2a305d3800de584a63f37
Kevin Berg <mail@kevinberg.de>
2019-07-10 09:05:03 +0200

    conf comments

--- src/config/role_perms.php
+++ src/config/role_perms.php
@@ -1,11 +1,21 @@
 <?php
 
 return [
-    'redirect_route_on_fail' => '/login', # redirect unauthorized users to this route (web guard).
+    /**
+     * Redirect unauthorized users to this route (web guard).
+     * Used for routes like /roles or /permissions.
+     */
+    'redirect_route_on_fail' => '/login',
 
+    /**
+     * Regex patterns for role and permission names.
+     */
     'permission_name_pattern' => '/^[a-zA-Z0-9-_. ]+$/',
     'role_name_pattern' => '/^[a-zA-Z0-9-_. ]+$/',
 
+    /**
+     * Cache keys for calculated roles and permissions.
+     */
     'roles_cache_key' => 'roles',
     'perms_cache_key' => 'perms'
 ];
